1c70f8f7a0d9a9a075a134f2d1f439e73c9b64e7
Iryna Shestak <shestak.irina@gmail.com>
2015-10-28 10:13:22 -0700

    given string, check if it's a tag/id/class :sparkles:

--- main.js
+++ main.js
@@ -23,6 +23,32 @@
  *   / |
  *  '  '
  */
-var cssquery = window.cssquery = function(selector) {
-    return [];
+var cssquery = window.cssquery = function(string) {
+  // split string + create array;
+  var selectors = string.split(" ");
+  var querySelectorAll;
+  // loop through param; 
+  [].forEach.call(selectors, function(selector) {
+    var byTagName = document.getElementsByTagName(selector);
+    var byClassName = document.getElementByClassName(selector);
+    var byId = document.getElementById(selector)
+    // 
+    querySelectorAll = checkIfASelector(byTagName, byClassName, byId);
+  });
+  return querySelectorAll;
 };
+
+// check if such a selector exists, push to given array;
+function checkIfASelector(querySelectorAll, byTagName, byClassName, byId) {
+  if (byTagName) {
+    querySelectorAll.push(byTagName);
+  }
+  if (byClassName) {
+    querySelectorAll(byClassName);
+  }
+  if (byId) {
+    querySelectorAll(byId)
+  }
+  return querySelectorAll;
+}
+
